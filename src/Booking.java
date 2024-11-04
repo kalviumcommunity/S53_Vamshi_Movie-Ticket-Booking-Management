45d218a13d9557b8fe72ec8d67b165941b349f8f
Vamshix57 <vamshineela06@gmail.com>
2024-11-04 15:17:25 +0530

    Done

--- src/Booking.java
+++ src/Booking.java
@@ -1,34 +1,15 @@
 public class Booking {
-    private Movie movie;         
-    private int numberOfTickets;
-    private static int totalTicketsBooked = 0;  
+    Movie movie;         
+    int numberOfTickets;   
 
-    // Accessor and Mutator for movie
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(Movie movie) {
-        this.movie = movie;
-    }
-
-    // Accessor and Mutator for numberOfTickets
-    public int getNumberOfTickets() {
-        return numberOfTickets;
-    }
-
-    public void setNumberOfTickets(int numberOfTickets) {
+    public void bookTicket(Movie movie, int numberOfTickets) {
+        this.movie = movie;  // Referencing dynamically allocated Movie object
         this.numberOfTickets = numberOfTickets;
-        totalTicketsBooked += numberOfTickets; 
     }
 
     public void displayBookingDetails() {
-        System.out.println("Booking Confirmed for: " + this.movie.getTitle());
-        System.out.println("Showtime: " + this.movie.getShowtime());
+        System.out.println("Booking Confirmed for: " + this.movie.title);
+        System.out.println("Showtime: " + this.movie.showtime);
         System.out.println("Number of Tickets: " + this.numberOfTickets);
-    }
-
-    public static int getTotalTicketsBooked() {
-        return totalTicketsBooked;
     }
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,53 +1,21 @@
 public class Movie {
-    private String title;
-    private String genre;
-    private String showtime;
-    private static int movieCount = 0;
+    String title;
+    String genre;
+    String showtime;
 
-    public Movie() {
-        movieCount++;
-    }
-
-    // Accessor and Mutator for title
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
+    public void setDetails(String title, String genre, String showtime) {
         this.title = title;
-    }
-
-    // Accessor and Mutator for genre
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
         this.genre = genre;
-    }
-
-    // Accessor and Mutator for showtime
-    public String getShowtime() {
-        return showtime;
-    }
-
-    public void setShowtime(String showtime) {
         this.showtime = showtime;
     }
 
-    public void setDetails(String title, String genre, String showtime) {
-        setTitle(title);
-        setGenre(genre);
-        setShowtime(showtime);
-    }
-
     public void displayDetails() {
-        System.out.println("Movie Title: " + getTitle());
-        System.out.println("Genre: " + getGenre());
-        System.out.println("Showtime: " + getShowtime());
+        System.out.println("Movie Title: " + this.title);
+        System.out.println("Genre: " + this.genre);
+        System.out.println("Showtime: " + this.showtime);
     }
 
-    public static int getTotalMovies() {
-        return movieCount;
+    public void showObjectCreated() {
+        System.out.println("A Movie object has been created.");
     }
 }
