26874c67326cefa8ff1690fce6a387bff29f48bc
Vamshix57 <vamshineela06@gmail.com>
2024-11-04 15:23:31 +0530

    Getters And Setters

--- src/Booking.java
+++ src/Booking.java
@@ -1,15 +1,32 @@
 public class Booking {
-    Movie movie;         
-    int numberOfTickets;   
+    private Movie movie;         
+    private int numberOfTickets;
+    private static int totalTicketsBooked = 0;  
 
-    public void bookTicket(Movie movie, int numberOfTickets) {
-        this.movie = movie;  // Referencing dynamically allocated Movie object
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public int getNumberOfTickets() {
+        return numberOfTickets;
+    }
+
+    public void setNumberOfTickets(int numberOfTickets) {
         this.numberOfTickets = numberOfTickets;
+        totalTicketsBooked += numberOfTickets; 
     }
 
     public void displayBookingDetails() {
-        System.out.println("Booking Confirmed for: " + this.movie.title);
-        System.out.println("Showtime: " + this.movie.showtime);
+        System.out.println("Booking Confirmed for: " + this.movie.getTitle());
+        System.out.println("Showtime: " + this.movie.getShowtime());
         System.out.println("Number of Tickets: " + this.numberOfTickets);
+    }
+
+    public static int getTotalTicketsBooked() {
+        return totalTicketsBooked;
     }
 }
