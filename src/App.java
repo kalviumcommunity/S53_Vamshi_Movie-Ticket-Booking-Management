5beb8269deebf1a7625fa014935ea0e2861b71ac
Vamshix57 <vamshineela06@gmail.com>
2024-11-08 10:53:02 +0530

    Constructors and Destructors

--- src/App.java
+++ src/App.java
@@ -1,30 +1,30 @@
 public class App {
     public static void main(String[] args) {
-        Movie[] movies = new Movie[3];  
+        Movie movie1 = new Movie();
+        movie1.setDetails("Inception", "Sci-Fi", "7:00 PM");
 
-        movies[0] = new Movie();
-        movies[0].setDetails("Inception", "Sci-Fi", "7:00 PM");
-
-        movies[1] = new Movie();
-        movies[1].setDetails("Titanic", "Romance", "9:00 PM");
-
-        movies[2] = new Movie();
-        movies[2].setDetails("Avatar", "Fantasy", "5:00 PM");
+        Movie movie2 = new Movie("Titanic", "Romance", "9:00 PM");
+        Movie movie3 = new Movie("Avatar", "Fantasy", "5:00 PM");
 
         System.out.println("\nAvailable Movies:");
-        for (Movie movie : movies) {
-            movie.displayDetails();
-            System.out.println();
-        }
+        movie1.displayDetails();
+        System.out.println();
+        movie2.displayDetails();
+        System.out.println();
+        movie3.displayDetails();
 
-        Booking booking = new Booking();
-        booking.setMovie(movies[1]);
-        booking.setNumberOfTickets(4);
+        Booking booking1 = new Booking();
+        booking1.setMovie(movie2);
+        booking1.setNumberOfTickets(4);
 
-        System.out.println("\nYour Booking:");
-        booking.displayBookingDetails();
+        Booking booking2 = new Booking(movie3, 2);
 
-        System.out.println("\nTotal Movies: " + Movie.getTotalMovies());
+        System.out.println("\nYour Bookings:");
+        booking1.displayBookingDetails();
+        System.out.println();
+        booking2.displayBookingDetails();
+
+        System.out.println("\nTotal Movies Created: " + Movie.getTotalMovies());
         System.out.println("Total Tickets Booked: " + Booking.getTotalTicketsBooked());
     }
 }
