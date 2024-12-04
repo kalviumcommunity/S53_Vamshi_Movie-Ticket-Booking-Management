db6580318024cea730a994cfc6a0b7e4361e68a5
Vamshix57 <vamshineela06@gmail.com>
2024-12-04 17:58:29 +0530

    Solid Priniciples 1

--- src/Booking.java
+++ src/Booking.java
@@ -1,13 +1,11 @@
 public class Booking extends AbstractTicket {
     private Movie movie;
     private int numberOfTickets;
-    private static int totalTicketsBooked = 0;
 
     public Booking(String ticketID, Movie movie, int numberOfTickets) {
         super(ticketID);
         this.movie = movie;
         this.numberOfTickets = numberOfTickets;
-        totalTicketsBooked += numberOfTickets;
     }
 
     public Movie getMovie() {
@@ -16,10 +14,6 @@ public class Booking extends AbstractTicket {
 
     public int getNumberOfTickets() {
         return numberOfTickets;
-    }
-
-    public static int getTotalTicketsBooked() {
-        return totalTicketsBooked;
     }
 
     @Override
